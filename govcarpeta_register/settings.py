d0c07fb71b8f403baa09acdc603e743b6fdf693f
Jean Pierre <jeanlondono09@gmail.com>
2023-09-25 21:27:04 -0500

    Add RDS connection

--- govcarpeta_register/settings.py
+++ govcarpeta_register/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$q2xuxmt43z4dmm3p)6udc&*qmu*l4pz%r@&=mf9s%d%$b!uz6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'govcarpeta_register.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'govcarpetadba',
+        'USER': 'postgres',
+        'PASSWORD': 'govcarpetadba',
+        'HOST': 'ec2-52-67-109-117.sa-east-1.compute.amazonaws.com', 
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
